bc1ebc40ad599d84adb1d8f39d7c8e56f802c170
stefani-nik <stefani_nik@abv.bg>
2018-06-23 10:44:29 +0300

    Initial commit

---
+++ src/ConsoleSnake/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleSnake
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
